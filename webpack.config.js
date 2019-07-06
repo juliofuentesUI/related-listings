1694c773553be53351f2cc4983426d8ab3ff7b67
Amar <amar.vadhia@gmail.com>
2019-07-05 18:50:27 -0700

    refactored script and config of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module : {
     rules: [
-      { test: /\.js|jsx$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   }
 }
