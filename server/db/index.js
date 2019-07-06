1c7be725103a0dfd21ffb4580de34ada992054ac
Amar <amar.vadhia@gmail.com>
2019-07-05 18:49:56 -0700

    moved database files into server folder

---
+++ server/db/index.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'related_listings'
+});
+
+connection.connect((err) => {
+  if(err){
+    console.log(err);
+    return;
+  } else {
+    console.log("connection secured");
+  }
+});
+
+module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,34 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./db/index');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
+app.use(bodyParser.urlencoded({ extended: false}));
 
-app.post('/rooms/listings', (req, res) => {
-
-});
-
-app.get('/rooms/listings', (req, res) => {
-
-});
-
-app.post('/rooms/listings/reviews/', (req, res) => {
-
-});
-
-app.get('/rooms/listings/reviews/', (req, res) => {
-  
-});
-
-app.post('/rooms/listings/user_info/', (req, res) => {
-  
-});
-
-app.get('/rooms/listings/user_info/', (req, res) => {
-  
+app.get('/rooms/related-listings', (req, res) => {
+  let queryString = "SELECT * FROM listings";
+  db.query(queryString, (err, rows, fields) => {
+    res.json(rows);
+  })
 });
 
 app.listen(PORT, () => {
