888c5e231e49eb88987a8aa03daf85eb8b02d9f0
Amar <amar.vadhia@gmail.com>
2019-07-06 13:13:26 -0700

    delete file. Get data to be passed in as state to App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,43 @@
 import React from "react";
+import Carousel from "./Carousel.jsx";
 
 class App extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
-
+      roomInfo: null
     }
+  }
+
+  componentDidMount(){
+    const reducedData = [];
+    fetch('/rooms/related-listings')
+      .then(res => res.json())
+      .then(json => {
+        json.forEach(data => {
+          if(reducedData.length <= 10){
+            reducedData.push(data);
+          }
+        })
+      })
+      this.setState({
+        roomInfo: reducedData
+      })
   }
 
   render(){
     return(
-      <div>
-        <h1>HELLO, WORLD, This is a test bruv</h1>
+      <div className="related-listings">
+        <div>
+          <span className="rooms-title">More places to stay</span>
+        </div>
+        <div>
+          {/* {this.state.roomInfo.map(singleInfo => {
+            <Carousel roomInfo={singleInfo} />
+          })} */}
+          <Carousel roomInfo={this.state.roomInfo}/>
+        </div>
       </div>
     )
   }
